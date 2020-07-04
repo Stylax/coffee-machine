8007810560f4cc9a8f254d33175ec00402a9e000
Stylax <aleksanderzawadzki0@gmail.com>
2020-07-04 03:42:51 +0200

    counting # of cups that machine can make

--- src/com/machine/Main.java
+++ src/com/machine/Main.java
@@ -7,13 +7,33 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int waterAmount = scanner.nextInt() / 200;
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int milkAmount = scanner.nextInt() / 50;
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int coffeeBeansAmount = scanner.nextInt() / 15;
         System.out.println("Write how many cups of coffee you will need:");
-        int coffeeNumber = scanner.nextInt();
-        System.out.println("For" + coffeeNumber + " cups of coffee you will need:");
-        System.out.println((coffeeNumber * 200) + "ml of water");
-        System.out.println((coffeeNumber * 50) + "ml of milk");
-        System.out.println((coffeeNumber * 15) + "g of coffee beans");
+        int coffeeCupsRequest = scanner.nextInt();
 
+        int maximumCoffies = Math.min(coffeeBeansAmount, Math.min(waterAmount,milkAmount));
+
+
+        if (maximumCoffies < coffeeCupsRequest){
+            System.out.println("No, I can make only " + maximumCoffies + " cup(s) of coffee");
+        } else if (maximumCoffies == coffeeCupsRequest) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else {
+            System.out.println("Yes, I can make that amount of coffee (and even " + (maximumCoffies - coffeeCupsRequest) + " more than that)");
+        }
+
+//        System.out.println("Write how many cups of coffee you will need:");
+//        int coffeeNumber = scanner.nextInt();
+//        System.out.println("For" + coffeeNumber + " cups of coffee you will need:");
+//        System.out.println((coffeeNumber * 200) + "ml of water");
+//        System.out.println((coffeeNumber * 50) + "ml of milk");
+//        System.out.println((coffeeNumber * 15) + "g of coffee beans");
+//
 
 //        System.out.println("Starting to make a coffee");
 //        System.out.println("Grinding coffee beans");
