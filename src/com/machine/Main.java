2f648aeffb594d3f122fad9064572119c61dc40b
Stylax <aleksanderzawadzki0@gmail.com>
2020-07-08 01:11:17 +0200

    filling a coffee machine, chosing a coffee, taking the money out

--- src/com/machine/Main.java
+++ src/com/machine/Main.java
@@ -7,25 +7,119 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Write how many ml of water the coffee machine has:");
-        int waterAmount = scanner.nextInt() / 200;
-        System.out.println("Write how many ml of milk the coffee machine has:");
-        int milkAmount = scanner.nextInt() / 50;
-        System.out.println("Write how many grams of coffee beans the coffee machine has:");
-        int coffeeBeansAmount = scanner.nextInt() / 15;
-        System.out.println("Write how many cups of coffee you will need:");
-        int coffeeCupsRequest = scanner.nextInt();
+        int waterLeft = 400;
+        int milkLeft = 540;
+        int beansLeft = 120;
+        int cupsLeft = 9;
+        int moneyLeft = 550;
 
-        int maximumCoffies = Math.min(coffeeBeansAmount, Math.min(waterAmount,milkAmount));
+        state(waterLeft, milkLeft, beansLeft, cupsLeft, moneyLeft);
 
+        System.out.println("Write action (buy, fill, take)");
+        String action = scanner.next();
 
-        if (maximumCoffies < coffeeCupsRequest){
-            System.out.println("No, I can make only " + maximumCoffies + " cup(s) of coffee");
-        } else if (maximumCoffies == coffeeCupsRequest) {
-            System.out.println("Yes, I can make that amount of coffee");
-        } else {
-            System.out.println("Yes, I can make that amount of coffee (and even " + (maximumCoffies - coffeeCupsRequest) + " more than that)");
+        switch (action) {
+            case "buy":
+                int[] coffeeAr = buy(waterLeft, milkLeft, beansLeft, moneyLeft);
+                waterLeft = coffeeAr[0];
+                milkLeft = coffeeAr[1];
+                beansLeft = coffeeAr[2];
+                moneyLeft = coffeeAr[3];
+                cupsLeft--;
+                state(waterLeft, milkLeft, beansLeft, cupsLeft, moneyLeft);
+                break;
+            case "fill":
+                int[] fillAr = fill(waterLeft, milkLeft, beansLeft, cupsLeft);
+                waterLeft = fillAr[0];
+                milkLeft = fillAr[1];
+                beansLeft = fillAr[2];
+                cupsLeft = fillAr[3];
+                state(waterLeft, milkLeft, beansLeft, cupsLeft, moneyLeft);
+                break;
+            case "take":
+                int money = take(moneyLeft);
+                moneyLeft -= money;
+                state(waterLeft, milkLeft, beansLeft, cupsLeft, moneyLeft);
+                break;
+            }
+    }
+
+    public static void state(int water, int milk, int beans, int cups, int money) {
+        System.out.println("The coffee machine has:");
+        System.out.println(water + " of water");
+        System.out.println(milk + " of milk");
+        System.out.println(beans + " of coffee beans");
+        System.out.println(cups + " of disposable cups");
+        System.out.println(money + " of money\n");
+    }
+
+    public static int[] buy(int water, int milk, int beans, int money) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino");
+        int coffeeType = scanner.nextInt();
+        System.out.println();
+
+        int[] coffeeAr = new int[4];
+
+        switch(coffeeType){
+            case 1:
+                coffeeAr[0] = water - 250;
+                coffeeAr[1] = milk;
+                coffeeAr[2] = beans - 16;
+                coffeeAr[3] = money + 4;
+                break;
+            case 2:
+                coffeeAr[0] = water - 350;
+                coffeeAr[1] = milk - 75;
+                coffeeAr[2] = beans - 20;
+                coffeeAr[3] = money + 7;
+                break;
+            case 3:
+                coffeeAr[0] = water - 200;
+                coffeeAr[1] = milk - 100;
+                coffeeAr[2] = beans - 12;
+                coffeeAr[3] = money + 6;
+                break;
         }
+        return coffeeAr;
+    }
+    public static int[] fill(int water, int milk, int beans, int cups) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] fillAr = new int[4];
+
+        System.out.println("Write how many ml of water do you want to add:");
+        fillAr[0] = scanner.nextInt() + water;
+        System.out.println("Write how many ml of milk do you want to add:");
+        fillAr[1] = scanner.nextInt() + milk;
+        System.out.println("Write how many grams of coffee beans do you want to add:");
+        fillAr[2] = scanner.nextInt() + beans;
+        System.out.println("Write how many disposable cups do you want to add:");
+        fillAr[3] = scanner.nextInt() + cups;
+        System.out.println();
+        return fillAr;
+    }
+    public static int take(int money) {
+        System.out.println("I gave you $" + money + "\n");
+
+        return money;
+    }
+}
+
+
+//        int coffeeCupsRequest = scanner.nextInt();
+//
+//        int maximumCoffees = Math.min(coffeeBeansAmount, Math.min(waterAmount,milkAmount));
+//
+//
+//        if (maximumCoffees < coffeeCupsRequest){
+//            System.out.println("No, I can make only " + maximumCoffees + " cup(s) of coffee");
+//        } else if (maximumCoffees == coffeeCupsRequest) {
+//            System.out.println("Yes, I can make that amount of coffee");
+//        } else {
+//            System.out.println("Yes, I can make that amount of coffee (and even " + (maximumCoffees - coffeeCupsRequest) + " more than that)");
+//        }
 
 //        System.out.println("Write how many cups of coffee you will need:");
 //        int coffeeNumber = scanner.nextInt();
@@ -42,5 +136,3 @@ public class Main {
 //        System.out.println("Pouring coffee into the cup");
 //        System.out.println("Pouring some milk into the cup");
 //        System.out.println("Coffee is ready!");
-    }
-}
